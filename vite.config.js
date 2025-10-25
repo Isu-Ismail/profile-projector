4ae6697435aeccbccf468bc0e8244fdf91570c7e
Ismail <ismailisims1@gmail.com>
2025-10-25 17:39:19 +0530

    Configure Vite and package.json for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  // This ensures assets are loaded from the correct GitHub Pages path:
+  // https://lsu-ismail.github.io/profile-projector/
+  base: '/profile-projector/', 
   plugins: [react()],
-})
+});
