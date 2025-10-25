333b2f6d2d60e5f1871c51a224a6adde8442c2d2
Ismail <ismailisims1@gmail.com>
2025-10-25 17:56:28 +0530

    done some router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter as Router, // This is BrowserRouter
+  HashRouter as Router, // CHANGED to HashRouter
   Routes,
   Route,
   Navigate,
@@ -12,33 +12,23 @@ import ProfileProjectorLabPage from "./experiments/ProfileProjector/LabPage";
 
 function App() {
   return (
-    // FIX: Add the basename prop to tell the router that
-    // the app is hosted under the /profile-projector/ path.
-    <Router basename="/profile-projector">
+    // FIX: Use HashRouter and REMOVE basename to fix 404s on refresh.
+    // All routes will now have a # prefix in the URL.
+    <Router>
       <Routes>
-        {/* --- Root Route Redirect: Send user to the Profile Projector Aim page --- 
-          NOTE: When using 'basename', all paths defined below are relative to it.
-          e.g., path="/" now means /profile-projector/
-          path="/experiments/profile-projector/Aim" now means /profile-projector/experiments/profile-projector/Aim
-        */}
-
-        {/* Since you have basename set, your initial path needs to be adjusted */}
-        {/* We can simplify the initial redirect since the basename covers the repo path */}
-
-        {/* --- Root Path Redirect (Handles: lsu-ismail.github.io/profile-projector/) --- */}
+        {/* --- Root Route Redirect --- */}
+        {/* Path "/" will match the URL ending in #/ */}
         <Route
           path="/"
           element={<Navigate to="/experiments/profile-projector/Aim" replace />}
         />
 
         {/* --- Main Experiment Information Routes --- */}
-        {/* Redirect: /experiments/profile-projector to /experiments/profile-projector/Aim */}
         <Route
           path="/experiments/profile-projector"
           element={<Navigate to="/experiments/profile-projector/Aim" replace />}
         />
 
-        {/* Renders the informational content (Aim, Theory, Procedure, Tests) */}
         <Route
           path="/experiments/profile-projector/:topic"
           element={<ProfileProjectorPage />}
